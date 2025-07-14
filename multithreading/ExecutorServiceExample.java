e9d6b3b6594df5b7b3551cbf5659b20e1b3dd375
Manas Mittal <manasmittal.ce@gmail.com>
2025-07-15 00:35:40 +0530

    ExecutorService code updated with some new info

--- multithreading/ExecutorServiceExample.java
+++ multithreading/ExecutorServiceExample.java
@@ -52,3 +52,18 @@ public class ExecutorServiceExample {
         executor.shutdown();  // Important: shut down the executor after use
     }
 }
+/*
+
+Some important point on shutdown() , shutdownNow() and awaitTermination(time, TimeUnit.unit)
+
+shutdown() is a non blocking call that is it doesnt blocks the main thread but after execution of this executor stops taking new tasks and when all the tasks gets finishes my executor object will not accept new tasks it can only be used to check status like executor.isDone().
+
+shutdownNow() calls Thread.interrupt on all the running threads in the pool hence if explicit check by isInterrupted or handling interruptedException is not used then running threads will keep running otherwise shoutdownNow interruptes all running threads and stops the executor from taking new tasks also it returns the size of list containing those tasks that are in the queue to be done.
+
+awaitTermination(time, TimeUnit.unit) -> this leads to block and wait main thread till executor finishes all the tasks. It should always be called after executor.shutdown().
+
+
+
+
+
+*/
